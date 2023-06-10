136bb2ea74df0f13679006a8c30a91add2c76465
YP <67188054+yesh21@users.noreply.github.com>
2023-06-10 18:59:25 +0530

    bug -- userscalable

--- zoom.js
+++ zoom.js
@@ -144,6 +144,27 @@ class Cell
         }
     };
 
+
+function StyleElements() {
+    document.querySelector(".zoom").style.height = Math.max(600, window.innerHeight) +'px';
+    document.getElementById("img-fit").style.height = Math.max(600, window.innerHeight) +'px';
+    document.getElementById("img-fit").style.width = Math.max(600, window.innerWidth) +'px';
+    document.getElementById("my-img").style.height = Math.max(600, window.innerHeight) +'px';
+    document.getElementById("canvas").style.height = Math.max(600, window.innerHeight) +'px';
+    document.getElementById("canvas").style.width = Math.max(600, window.innerWidth) +'px';
+    
+    //let diff = window.innerWidth - window.innerHeight;
+    
+    if(window.innerWidth > 600){
+        //document.getElementById("bbig").style.scale = 1+(diff/1200);
+        document.body.style.overflowX = 'hidden';
+        document.querySelector(".color_chg-btn").style.right = '1px';
+    } else {
+        document.body.style.overflowX = 'visible';
+        document.querySelector(".color_chg-btn").style.right = (window.innerWidth-600)*100/window.innerWidth + '%' ;
+    }
+}
+
 window.onload = function() {
   setTimeout(function () {
     document.getElementById("preloader").remove();
@@ -153,42 +174,12 @@ window.onload = function() {
     start = true;
 }, 10000);
 
-document.querySelector(".zoom").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("img-fit").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("img-fit").style.width = Math.max(600, window.innerWidth) +'px';
-document.getElementById("my-img").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("canvas").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("canvas").style.width = Math.max(600, window.innerWidth) +'px';
-
-let diff = window.innerWidth - window.innerHeight;
-
-if(window.innerWidth > 600){
-    //document.getElementById("bbig").style.scale = 1+(diff/1200);
-    document.body.style.overflowX = 'hidden';
-} else {
-    document.body.style.overflowX = 'visible';
-    document.querySelector(".color_chg-btn").style.right = (window.innerWidth-600)*100/window.innerWidth + '%' ;
-}
+StyleElements();
 
 }
 
 window.onresize = function() {
-document.querySelector(".zoom").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("img-fit").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("img-fit").style.width = Math.max(600, window.innerWidth) +'px';
-document.getElementById("my-img").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("canvas").style.height = Math.max(600, window.innerHeight) +'px';
-document.getElementById("canvas").style.width = Math.max(600, window.innerWidth) +'px';
-
-let diff = window.innerWidth - window.innerHeight;
-
-if(window.innerWidth > 600){
-    //document.getElementById("bbig").style.scale = 1+(diff/1200);
-    document.body.style.overflowX = 'hidden';
-} else {
-    document.body.style.overflowX = 'visible';
-    document.querySelector(".color_chg-btn").style.right = (window.innerWidth-600)*100/window.innerWidth + '%' ;
-}
+    StyleElements();
 }
 
 
