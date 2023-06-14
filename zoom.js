f1bdcf4d8b032ade511a037e9ae676e292df01fd
YP <67188054+yesh21@users.noreply.github.com>
2023-06-14 11:38:55 +0530

    Add files via upload

--- zoom.js
+++ zoom.js
@@ -36,7 +36,7 @@ StyleElements();
 window.onresize = function() {
     StyleElements();
     //document.querySelector(".main").style.width =  Math.max(600, window.innerWidth) +'px';
-    document.body.style.width = "100%";
+    //document.body.style.width = "100%";
     //document.querySelector('.cursor__ball--big').style.transform = "translate(150px, 38vh)";
   //  window.scrollBy(0, 1); // 0 pixels horizontal and 1 pixel down
 //window.scrollBy(0, -1); // 0 pixels horizontal and 1 pixel up
