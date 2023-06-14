be5ee04e1876db661d21b25bd17fbb900369c7cb
YP <67188054+yesh21@users.noreply.github.com>
2023-06-14 14:43:05 +0530

    Add files via upload

--- zoom.js
+++ zoom.js
@@ -15,10 +15,10 @@ function StyleElements() {
     
     if(window.innerWidth > 600){
         //document.getElementById("bbig").style.scale = 1+(diff/1200);
-        document.body.style.overflowX = 'clip';
+        document.body.style.overflowX = 'hidden';
         document.querySelector(".color_chg-btn").style.right = '1px';
     } else {
-        document.body.style.overflowX = 'clip'; // remember
+        document.body.style.overflowX = 'visible'; // remember
         document.querySelector(".color_chg-btn").style.right = (window.innerWidth-600)*100/window.innerWidth + '%' ;
     }
 }
@@ -35,7 +35,7 @@ StyleElements();
 
 window.onresize = function() {
     StyleElements();
-    //document.querySelector(".main").style.width =  Math.max(600, window.innerWidth) +'px';
+    document.body.style.width =  '1000px';
     //document.body.style.width = "100%";
     //document.querySelector('.cursor__ball--big').style.transform = "translate(150px, 38vh)";
   //  window.scrollBy(0, 1); // 0 pixels horizontal and 1 pixel down
