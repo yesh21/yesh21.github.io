8944de27d6e9437e05eeddf111bb2aec24ea63ff
YP <67188054+yesh21@users.noreply.github.com>
2023-06-19 15:09:12 +0530

    added scrolltrigger

--- zoom.js
+++ zoom.js
@@ -18,7 +18,7 @@ function StyleElements() {
         document.body.style.overflowX = 'hidden';
         document.querySelector(".color_chg-btn").style.right = '1px';
     } else {
-        document.body.style.overflowX = 'hidden'; // remember
+        document.body.style.overflowX = 'visible'; // remember
         document.querySelector(".color_chg-btn").style.right = (window.innerWidth-600)*100/window.innerWidth + '%' ;
     }
 }
@@ -33,13 +33,38 @@ setTimeout(function () {
  ScrollTrigger.refresh()
 
       gsap.registerPlugin(ScrollTrigger);
-      const tl = gsap.timeline( {
-  scrollTrigger: {
+
+      const tl1 = gsap.timeline( {
+        scrollTrigger: {
+          normalizeScroll: true,
+          trigger: ".qwer",
+          scrub: 1,
+          //markers: true,
+          pin: ".qwer",
+          ease: "elastic(2.5, 1)",
+          start: "top top",
+          end: "+=500%"
+        }
+      })
+      
+      //tl.totalDuration(20); 
+      
+        tl1
+        //.to('.qwer', { scale: 10,  ease: "power1.inOut"})
+        .to('.center', { opacity: 0,  ease: "power1.inOut"})
+        .to('#trigger1', {containerAnimation: tween	})
+        //normalizeScroll
+        //.to('#bbig', { height: "100vh", width: "100vw", ease: "power1.inOut"})
+
+
+    const tl = gsap.timeline( {
+    scrollTrigger: {
     normalizeScroll: true,
     trigger: ".orange",
     scrub: 1,
     //markers: true,
     pin: ".orange",
+    ease: "elastic(2.5, 1)",
     start: "top top",
     end: "+=500%"
   }
@@ -49,11 +74,12 @@ setTimeout(function () {
 
   tl
   //.to('.center', { opacity: 0 })
-  .to('.intro-text-bold', { delay: 15 })
+  .from('.aboutme', { xPercent:-100, duration: 5, ease:'power2.out' })
    .to('.intro-text-bold', { opacity: 0.1, duration: 10})
    //.from(".intro-text-bold",  { y: innerHeight * 1.5 })
    .to('.intro-text', {fontSize: "10vmax", duration: 10}) //color: "darkcyan",
    .to('.intro-text-bold', { delay: 10 })
+   //.from('.aboutme', { xPercent:100, duration: 5, ease:'power2.out' })
    //.from(".intro-text",  { y: innerHeight * 1.5 })
 
 }, 10000);
@@ -62,6 +88,7 @@ setTimeout(function () {
 StyleElements();
 
 }
+
 
 window.onresize = function() {
     StyleElements();
