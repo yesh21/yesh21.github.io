41770174fe07b480d6e03441f437559fc9044c74
yaswanth Pulavarthi <sc19yp@leeds.ac.uk>
2023-06-20 14:54:56 +0530

    responsiveness

--- zoom.js
+++ zoom.js
@@ -15,7 +15,7 @@ function StyleElements() {
     
     if(window.innerWidth > 700){
         //document.getElementById("bbig").style.scale = 1+(diff/1200);
-        document.body.style.overflowX = 'hidden';
+        //document.body.style.overflowX = 'hidden';
         //document.querySelector(".color_chg-btn").style.right = '1px';
         document.querySelector(".color_chg-btn").style.scale = '1';
         document.querySelector(".color_chg-btn").style.padding = '6%';
