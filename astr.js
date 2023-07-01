9e142050f0e7187b750f59fda027d73cb7e1b57e
yaswanth Pulavarthi <sc19yp@leeds.ac.uk>
2023-07-01 20:45:57 +0530

    astroid changes

--- astr.js
+++ astr.js
@@ -19,23 +19,6 @@ light.position.set(ww/4, 0, wh/2 );
    var light2 = new THREE.PointLight( 0x6495ed, 6, 1000 );
 light2.position.set(0, 0, 0 );
 	scene.add(light2);
-
-  var material = new THREE.PointCloudMaterial({
-      color: 0x333333
-    });
-    
-    var geometry = new THREE.Geometry();
-    var x, y, z;
-    for(var i=0;i<2000;i++){
-      x = ww/2-(Math.random() * ww);
-      y = wh/2-(Math.random() * wh);
-      z = -100;
-      
-      geometry.vertices.push(new THREE.Vector3(x, y, z));
-    };
-    
-    var pointCloud = new THREE.PointCloud(geometry, material);
-    scene.add(pointCloud);
   
   var asteroids = createAsteroids();
   
@@ -45,6 +28,10 @@ light2.position.set(0, 0, 0 );
           obj.rotation.y -= obj.r.y;
           obj.rotation.z -= obj.r.z;
     })
+    //const timer = 0.001 * Date.now();
+    //camera.lookAt(scene.position);
+    //camera.position.x = Math.sin(timer /2 ) * -20;
+    //camera.position.z = Math.cos(timer / 2) * 20;
 
     renderer.render(scene, camera);
     requestAnimationFrame(update);
@@ -56,14 +43,12 @@ function createAsteroids(){
   var maxWidth = 1000;
   var asteroids = [];
   for(var i=0;i<100;i++){
-    asteroids.push(createRock(wh/30,ww,maxWidth,300,400));
+    asteroids.push(createRock(wh/30,ww));
   }
   return asteroids;
 }
 
-
-
-function createRock(size,spreadX,maxWidth,maxHeight,maxDepth){
+function createRock(size,spreadX){
 	geometry = new THREE.DodecahedronGeometry(size, 1);
   geometry.vertices.forEach(function(v){
     v.x += (0-Math.random()*(size/4));
@@ -83,7 +68,6 @@ function createRock(size,spreadX,maxWidth,maxHeight,maxDepth){
   cube.receiveShadow = true;
   cube.scale.set(1+Math.random()*0.4,1+Math.random()*0.8,1+Math.random()*0.4);
   var x = spreadX/2-Math.random()*spreadX;
-  var centeredness = 1-(Math.abs(x)/(maxWidth/2));
   var y = wh/2-(Math.random() * wh);
   var z = wh/2-(Math.random() * wh);
   cube.position.set(x,y,z)
